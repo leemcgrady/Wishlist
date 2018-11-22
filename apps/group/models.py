1da48fba0b8b0576b269b015c1ce7bbee6e42176
lige_ubuntu <1415957621@qq.com>
2018-11-22 20:37:10 +0800

    设计数据库

--- apps/group/models.py
+++ apps/group/models.py
@@ -1,34 +1,36 @@
-# from django.db import models
-# from db.base_model import BaseModel
-# from user.models import User
-#
-# class Group(BaseModel):
-#     '''分组模型类'''
-#
-#     group_name = models.CharField(max_length=20, verbose_name='分组名称')
-#     desc = models.CharField(max_length=256, verbose_name='分组简介')
-#     # image = models.ImageField(upload_to='goods', verbose_name='分组图片')
-#
-#     class Meta:
-#         db_table = 'wl_group'
-#         verbose_name = '分组'
-#         verbose_name_plural = verbose_name
-#
-#
-# class GroupUser(BaseModel):
-#
-#     '''分组用户模型类'''
-#
-#     status_choices = (
-#         (0, '下线'),
-#         (1, '上线'),
-#     )
-#     group = models.ForeignKey('Group', on_delete=models.CASCADE, verbose_name='所属组')
-#     user_id = models.ForeignKey('User', on_delete=models.CASCADE, verbose_name='用户id')
-#
-#     status = models.SmallIntegerField(default=1, choices=status_choices, verbose_name='用户状态')
-#
-#     class Meta:
-#         db_table = 'wl_group_user'
-#         verbose_name = '分组用户'
-#         verbose_name_plural = verbose_name
+from django.db import models
+from db.base_model import BaseModel
+from user.models import User
+
+
+class Group(BaseModel):
+
+    '''分组模型类'''
+
+    name = models.CharField(max_length=20, verbose_name='分组名称')
+    create_user = models.ForeignKey('User', on_delete=models.CASCADE, verbose_name='创建者')
+    desc = models.CharField(max_length=256, verbose_name='分组简介')
+    image = models.ImageField(upload_to='group', verbose_name='分组图片')
+
+    class Meta:
+        db_table = 'wl_group'
+        verbose_name = '分组'
+        verbose_name_plural = verbose_name
+
+
+class GroupUser(BaseModel):
+
+    '''分组用户模型类'''
+
+    status_choices = (
+        (0, '已加入'),
+        (1, '被移除'),
+    )
+    group = models.ForeignKey('Group', on_delete=models.CASCADE, verbose_name='所属组')
+    user = models.ForeignKey('User', on_delete=models.CASCADE, verbose_name='组内用户')
+    status = models.SmallIntegerField(default=0, choices=status_choices, verbose_name='用户状态')
+
+    class Meta:
+        db_table = 'wl_group_user'
+        verbose_name = '分组用户'
+        verbose_name_plural = verbose_name
--- apps/wish/models.py
+++ apps/wish/models.py
@@ -1,5 +1,27 @@
-# from django.db import models
-# from db.base_model import BaseModel
-#
-# class Wish(BaseModel):
-#     pass
+from django.db import models
+from db.base_model import BaseModel
+from group.models import Group
+from user.models import User
+
+
+class Wish(BaseModel):
+
+    status_choices = (
+        (0, "待抽取"),
+        (1, "待完成"),
+        (2, "已完成"),
+        (3, "已取消"),
+    )
+    name = models.CharField(max_length=20, verbose_name='愿望名称')
+    desc = models.CharField(max_length=256, verbose_name='愿望详情')
+    group = models.ForeignKey('Group', on_delete=models.CASCADE, verbose_name='所属分组')
+    create_user = models.ForeignKey('User', on_delete=models.CASCADE, verbose_name='创建者')
+    value = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='愿望价值')
+    weight = models.SmallIntegerField(default=1, choices=(0, 10), verbose_name="愿望权重")
+    status = models.SmallIntegerField(default=0, choices=status_choices, verbose_name='愿望状态')
+    end_time = models.DateTimeField(verbose_name='截止时间')
+
+    class Meta:
+        db_table = 'wl_wish'
+        verbose_name = '愿望'
+        verbose_name_plural = verbose_name
